3e65a56f0e0a3ab142da1ce0ac12a0bdfbd21611
spiralbot <ci-bot@spiralscout.com>
2024-11-25 20:19:45 +0000

    prepare release

--- src/Context/TargetInterface.php
+++ src/Context/TargetInterface.php
@@ -48,7 +48,7 @@ interface TargetInterface extends Stringable
      *
      * If the object is present, it always corresponds to the method reflection from {@see getReflection()}.
      *
-     * @return TController
+     * @return TController|null
      */
     public function getObject(): ?object;
 
