eef06edbd41d328d3f5de898fb1f096e575b8f3d
spiralbot <ci-bot@spiralscout.com>
2026-09-24 10:39:29 +0000

    Fix psalm notice after composer update (#1306)

--- src/Exception/InterceptorException.php
+++ src/Exception/InterceptorException.php
@@ -6,4 +6,5 @@ namespace Spiral\Interceptors\Exception;
 
 class InterceptorException extends \RuntimeException {}
 
+/** @psalm-suppress UndefinedClass */
 \class_alias(InterceptorException::class, \Spiral\Core\Exception\InterceptorException::class);
